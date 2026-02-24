718184ac18093ef1840e6bb50db4070ce0ddcb79
MEG315Group7 <abicity2005@gmail.com>
2026-02-24 20:48:04 +0100

    Delete Group7 MEG 315 directory

--- Group7 MEG 315/start_adhtc.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Starting AD-HTC System...
-cd /d "C:\Users\JDB-JAKE\Documents\trae_projects\230404035 ASS7"
-python launcher_fixed.py
-pause
--- Group7 MEG 315/start_python_frontend.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Starting Python Frontend Only...
-cd /d "C:\Users\JDB-JAKE\Documents\trae_projects\230404035 ASS7rontend_python"
-python adhtc_gui_simple.py
-pause
